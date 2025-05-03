27c6e90ea3f9515c39a86217a1b05a6dd28455f4
jhmfreitas <jhmfreitas1998@gmail.com>
2025-05-03 19:02:38 +0200

    Add reward network implementation and Spring configuration
    Implemented `RewardNetworkImpl` logic for processing dining rewards. Added Spring Java configuration in `RewardsConfig` to define the required beans and integrated it with test infrastructure and unit tests.

--- 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ 12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,6 +1,17 @@
 package config;
 
 import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
 
 /**
  * TODO-00: In this lab, you are going to exercise the following:
@@ -41,10 +52,40 @@ import javax.sql.DataSource;
  * - Note that return type of each bean method should be an interface
  *   not an implementation.
  */
-
+@Configuration
 public class RewardsConfig {
 
 	// Set this by adding a constructor.
 	private DataSource dataSource;
+
+	public RewardsConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Bean
+	public RewardNetwork rewardNetwork() {
+		return new RewardNetworkImpl(accountRepository(), restaurantRepository(), rewardRepository());
+	}
+
+	@Bean
+	public AccountRepository accountRepository() {
+		JdbcAccountRepository jdbcAccountRepository = new JdbcAccountRepository();
+		jdbcAccountRepository.setDataSource(dataSource);
+		return jdbcAccountRepository;
+	}
+
+	@Bean
+	public RestaurantRepository restaurantRepository() {
+		JdbcRestaurantRepository jdbcRestaurantRepository = new JdbcRestaurantRepository();
+		jdbcRestaurantRepository.setDataSource(dataSource);
+		return jdbcRestaurantRepository;
+	}
+
+	@Bean
+	public RewardRepository rewardRepository() {
+		JdbcRewardRepository jdbcRewardRepository = new JdbcRewardRepository();
+		jdbcRewardRepository.setDataSource(dataSource);
+		return jdbcRewardRepository;
+	}
 
 }
